d388e1d54450b58787d5d49b1aa0b49cd5ac24ae
pirollicrandle890 <pirollicrandle890@gmail.com>
2025-03-17 11:41:14 +0000

    Add 12.ts

---
+++ src/12.ts
@@ -0,0 +1,16 @@
+// Generated on Tue, 02 Mar 2021 22:38:47 GMT
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mathclass',
+  templateUrl: './mathclass.component.html',
+  styleUrls: ['./mathclass.component.scss']
+})
+export class MathclassComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
