6599ef019b37aee3299c81edd477acf7401dd510
pirollicrandle890 <pirollicrandle890@gmail.com>
2025-03-18 16:26:46 +0000

    Add 13.ts

---
+++ src/13.ts
@@ -0,0 +1,5 @@
+// Generate a random number between 1 and 10
+const randomNumber = Math.floor(Math.random() * 10) + 1;
+
+// Print the result to the console
+console.log(`The generated random number is: ${randomNumber}`);
