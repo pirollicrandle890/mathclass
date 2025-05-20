4f5b3286e511519dfd792f95a4ac0800b13536b1
pirollicrandle890 <pirollicrandle890@gmail.com>
2025-05-20 06:37:51 +0000

    Add 38.ts

---
+++ src/38.ts
@@ -0,0 +1,8 @@
+// Example 1: A simple example of an asynchronous function
+async function calculateArea() {
+    const width = 10;
+    const height = 5;
+    let area = width * height;
+    console.log(`The area is ${area}`);
+}
+calculateArea();
